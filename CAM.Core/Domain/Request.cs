bdbb975f10a2815720d0b497e6351915c6aa60f6
Alan Lai <anlai@ucdavis.edu>
2012-06-28 15:54:32 -0700

    fixed some sizing stuff and the default date on the date picker

--- CAM.Core/Domain/Request.cs
+++ CAM.Core/Domain/Request.cs
@@ -1,11 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using UCDArch.Core.DomainModel;
 
 namespace CAM.Core.Domain
 {
-    public class Request
+    public class Request : DomainObject
     {
+        public Request()
+        {
+            Start = DateTime.Now;
+        }
+
+        [Required]
+        [StringLength(50)]
+        public virtual string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string PositionTitle { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Department { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Unit { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string OfficeLocation { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Room { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Phone]
+        public string ContactPhone { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime? End { get; set; }
+
+        public PositionType Type { get; set; }
     }
+
+    public enum PositionType { NewPosition = 0, Rehire = 1};
 }
---
+++ CAM/Controllers/RequestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CAM.Core.Domain;
+
+namespace CAM.Controllers
+{
+    public class RequestController : Controller
+    {
+        public ActionResult Index()
+        {
+            var request = new Request();
+            return View(request);
+        }
+
+    }
+}
