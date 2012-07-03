0ce299a66dd37fd0b0b456ec28983580bef40dd5
anlai <anlai@ucdavis.edu>
2012-07-03 10:12:05 -0700

    changed lists for software/network
    made it so that there is a "restricted" list per profile and a seperate
    list for the pre-selected.

--- CAM/Models/RequestViewModel.cs
+++ CAM/Models/RequestViewModel.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using CAM.Core.Domain;
 using CAM.Core.Repositories;
 
@@ -17,9 +18,7 @@ namespace CAM.Models
         {
             var viewModel = new RequestViewModel()
                 {
-                    Request = new Request(),
-                    Softwares = repositoryFactory.SoftwareRepository.Queryable.Where(a => a.Site == site).ToList(),
-                    NetworkShares = repositoryFactory.NetworkShareRepository.Queryable.Where(a => a.Site == site).ToList()
+                    Request = new Request()
                 };
 
             if (request == null)
@@ -31,11 +30,34 @@ namespace CAM.Models
     
                     // copy the values
                     viewModel.Request = new Request(template);
+
+                    viewModel.Softwares = template.AvailableSoftware;
+                    viewModel.NetworkShares = template.AvailableNetworkShares;
+                }
+                else
+                {
+                    viewModel.Softwares = repositoryFactory.SoftwareRepository.Queryable.Where(a => a.Site == site).ToList();
+                    viewModel.NetworkShares = repositoryFactory.NetworkShareRepository.Queryable.Where(a => a.Site == site).ToList();
                 }
             }
 
             return viewModel;
         }
 
+        public IEnumerable<SelectListItem> GetSoftwareList()
+        {
+            return Softwares.Select(s => new SelectListItem() { Selected = Request.Software.Contains(s), Text = s.Name, Value = s.Id.ToString() }).ToList();
+        }
+
+        public IEnumerable<CustomSelect> GetNetworkShareList()
+        {
+            return NetworkShares.Select(n => new CustomSelect() {Selected = Request.NetworkShares.Contains(n), Text = n.Name, Value = n.Id.ToString(), ForceSelect = n.ForceSelect}).ToList();
+        }
+
+    }
+
+    public class CustomSelect : SelectListItem
+    {
+        public bool ForceSelect { get; set; }
     }
 }
