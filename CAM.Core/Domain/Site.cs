67874ac77b9058a462067bc5d71b9c8cc954e72f
anlai <anlai@ucdavis.edu>
2012-07-11 11:28:33 -0700

    added ad fields to site.

--- CAM.Core/Domain/Site.cs
+++ CAM.Core/Domain/Site.cs
@@ -15,6 +15,10 @@ namespace CAM.Core.Domain
         public virtual string Name { get; set; }
         public virtual IList<Unit> Units { get; set; }
 
+        public virtual string ActiveDirectoryServer { get; set; }
+        public virtual string SecurityGroupOu { get; set; }
+        public virtual string UsersOu { get; set; }
+
         public virtual bool HasUnits()
         {
             return Units.Any();
@@ -29,6 +33,9 @@ namespace CAM.Core.Domain
 
             Map(x => x.Name);
             HasMany(x => x.Units);
+            Map(x => x.ActiveDirectoryServer);
+            Map(x => x.SecurityGroupOu);
+            Map(x => x.UsersOu);
         }
     }
 }
--- CAM.Db/dbo/Tables/Sites.sql
+++ CAM.Db/dbo/Tables/Sites.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[Sites] (
     [Id]   VARCHAR (5)  NOT NULL,
     [Name] VARCHAR (50) NOT NULL,
+    [ActiveDirectoryServer] VARCHAR(100) NULL, 
+    [SecurityGroupOu] VARCHAR(MAX) NULL, 
+    [UserOu] VARCHAR(MAX) NULL, 
     CONSTRAINT [PK_Sites] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
